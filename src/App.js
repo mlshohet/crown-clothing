04320e06213e6e05e8db5453df4900e755a035a8
Mike Shohet <mshohet@Mikes-MacBook-Pro.local>
2020-09-10 14:21:20 -0400

    added navigation and reactor-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'; //Routing form React
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+    <div>
+      <h1>HATS </h1>
+    </div>
+  );
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
