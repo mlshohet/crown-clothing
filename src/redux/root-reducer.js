3d5ddb4eb8561b003b81ad4100bd8b667d17d31a
Mike Shohet <mshohet@Mikes-MacBook-Pro.local>
2020-09-17 02:42:46 -0400

    routed shop to individual collections, and moved data to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,32 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist'; //importing Redux Persist
+
+//importing Redux Persist storage
+import storage from 'redux-persist/lib/storage';
+
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducers';
 
-export default combineReducers({
+//config for Redux persist (JSON object)
+const persistConfig = {
+	key: 'root',
+	storage,
+	//String names of any reducer to be stored.
+	//User reducer is being handled by Firebase
+	whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
 	user: userReducer,
-	cart: cartReducer
-});
+	cart: cartReducer,
+	directory: directoryReducer,
+	shop: shopReducer
+});	
+
+//modified root reducer with persist capabilities
+export default persistReducer(persistConfig, rootReducer);
+
+
