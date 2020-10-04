c49b45dc6b81a97f8b6efd00ec67fc1df28bd3b9
Mike Shohet <mlshohet@gmail.com>
2020-10-03 21:51:43 -0400

    converted thunks to sagas in the shop redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import logger from 'redux-logger';
 
+import { fetchCollectionsStart } from './shop/shop.sagas';
+
 import rootReducer from './root-reducer';
 
-const middlewares = [thunk]; //add all middleware here
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware]; //add all middleware here
 
 if (process.env.NODE_ENV === 'development'){
 	middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares)); // you don't have to add middleware here
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
 
