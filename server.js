e5e992d024bfa249b9cec0fe6716ec359b91022c
Mike Shohet <mlshohet@gmail.com>
2020-10-20 17:29:27 -0400

    deleted debugging console logs

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ if (process.env.NODE_ENV === 'production') {
 
 app.listen(port, error => {
 	if (error) throw error;
-	console.log('Server running on port ' + port);
 });
 
 app.post('/payment', (req, res) => {
