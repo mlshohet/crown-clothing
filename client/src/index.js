c4c9c00c5fd3da6834f5216c05b8ef3399b929b8
Mike Shohet <mlshohet@gmail.com>
2020-10-28 16:34:26 -0400

    update for PWA compliance, added serviceworker, https, updated manifest with icons

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'; //React Persist
 
 import { store, persistor } from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
@@ -19,4 +20,8 @@ ReactDOM.render(
  	</Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
+
+
 
